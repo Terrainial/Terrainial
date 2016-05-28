9d31e4c05c2630b4275a71e1cdb711a00317c23c
Prashanna Raghavan <prashanna.raghavan@gmail.com>
2016-05-28 13:24:52 -0700

    Initial Commit

---
+++ Terrainial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Terrainial
+//
+//  Created by student on 5/28/16.
+//  Copyright © 2016 Terrainial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Terrainial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Terrainial
+//
+//  Created by student on 5/28/16.
+//  Copyright © 2016 Terrainial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
